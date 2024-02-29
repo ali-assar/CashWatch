4f1c0f7bd17a2c564c19738ae016ac4d94616659
ali <ali.assar@skiff.com>
2024-02-29 19:38:28 +0330

    adding user methods to gate

--- api-gateway/gate.go
+++ api-gateway/gate.go
@@ -22,14 +22,51 @@ func main() {
 
 	userClient := pb.NewUserServiceClient(conn)
 
-	app.Get("/users/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
+	app.Get("/user/:email", func(c *fiber.Ctx) error {
+		email := c.Params("email")
 
-		resp, err := userClient.GetUserByEmail(context.Background(), &pb.UserRequest{Email: id})
+		resp, err := userClient.GetUserByEmail(context.Background(), &pb.UserRequest{Email: email})
 		if err != nil {
 			return c.Status(http.StatusInternalServerError).SendString("Error fetching user")
 		}
 		return c.JSON(resp)
+	})
+
+	app.Post("/user", func(c *fiber.Ctx) error {
+		var params pb.User
+		if err := c.BodyParser(&params); err != nil {
+			return c.Status(http.StatusBadRequest).SendString("failed to parse the body")
+		}
+		resp, err := userClient.InsertUser(context.Background(), &params)
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).SendString("Error posting user")
+		}
+		return c.JSON(resp)
+
+	})
+
+	app.Delete("/user/:email", func(c *fiber.Ctx) error {
+		email := c.Params("email")
+
+		_, err := userClient.DeleteUserByEmail(context.Background(), &pb.UserRequest{Email: email})
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).SendString("Error deleting user")
+		}
+		return nil
+	})
+
+	app.Put("/user/:email", func(c *fiber.Ctx) error {
+		var params pb.UpdateUserRequest
+		if err := c.BodyParser(&params); err != nil {
+			return c.Status(http.StatusBadRequest).SendString("failed to parse the body")
+		}
+		params.Email = c.Params("email")
+
+		_, err := userClient.UpdateUserByEmail(context.Background(), &params)
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).SendString("Error updating user")
+		}
+		return nil
 	})
 
 	err = app.Listen(":8080")
